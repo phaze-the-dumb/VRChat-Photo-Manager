8cb2d10e95982c00ee8402dc15de176466829382
phaze-the-dumb <phaze@duck.com>
2023-08-19 15:24:04 +0100

    fix ui just being shit and not working

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.on('ready', () => {
   if(isDev = process.env.APP_DEV ? (process.env.APP_DEV.trim() == "true") : false)
     mainWindow.loadURL('http://localhost:5173/');
   else
-    mainWindow.loadFile(path.join(__dirname, '../../../ui/index.html'));
+    mainWindow.loadFile(path.join(__dirname, './gui/dist/index.html'));
 
   mainWindow.webContents.on('did-start-navigation', () => photo.allowAuth.allow = true);
 });
