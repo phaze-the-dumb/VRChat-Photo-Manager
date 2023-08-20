2b460cf1765bb4a3a83e4f1d9f66626ce6d041f3
phaze-the-dumb <phaze@duck.com>
2023-08-20 11:09:18 +0100

    closes #18 window position now saves

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const fs = require('fs');
+const os = require('os');
 
 let photo = require('./photoServer.js');
+let windowRect = () => [ 0, 0, 1160, 700 ];
+
+if(!fs.existsSync(os.homedir() + '/AppData/Roaming/PhazeDev/.config/'))
+  fs.mkdirSync(os.homedir() + '/AppData/Roaming/PhazeDev/.config/', { recursive: true });
+
+if(!fs.existsSync(os.homedir() + '/AppData/Roaming/PhazeDev/.config/vrcphotos.json'))
+  fs.writeFileSync(os.homedir() + '/AppData/Roaming/PhazeDev/.config/vrcphotos.json', '{}');
+
+let config = JSON.parse(fs.readFileSync(os.homedir() + '/AppData/Roaming/PhazeDev/.config/vrcphotos.json', 'utf8'));
+
+if(!config.rect){
+  config.rect = windowRect();
+  fs.writeFileSync(os.homedir() + '/AppData/Roaming/PhazeDev/.config/vrcphotos.json', JSON.stringify(config));
+}
 
 app.on('ready', () => {
   let mainWindow = new BrowserWindow({
-    width: 1160,
-    height: 700
+    x: config.rect[0],
+    y: config.rect[1],
+    width: config.rect[2],
+    height: config.rect[3],
   });
 
+  let bounds = mainWindow.getBounds();
   mainWindow.setMenuBarVisibility(false);
 
   if(isDev = process.env.APP_DEV ? (process.env.APP_DEV.trim() == "true") : false)
@@ -17,8 +36,18 @@ app.on('ready', () => {
     mainWindow.loadFile(path.join(__dirname, '../ui/index.html'));
 
   mainWindow.webContents.on('did-start-navigation', () => photo.allowAuth.allow = true);
+
+  windowRect = () => {
+    return [ bounds.x, bounds.y, bounds.width, bounds.height ];
+  }
+
+  mainWindow.on('resize', () => bounds = mainWindow.getBounds());
+  mainWindow.on('moved', () => bounds = mainWindow.getBounds());
 });
 
 app.on('window-all-closed', () => {
+  config.rect = windowRect();
+  fs.writeFileSync(os.homedir() + '/AppData/Roaming/PhazeDev/.config/vrcphotos.json', JSON.stringify(config));
+
   app.quit();
 });
