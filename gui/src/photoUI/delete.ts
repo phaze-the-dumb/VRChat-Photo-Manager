4d6c09564b91f34d8b563d883c592b0104a4d7c7
phaze-the-dumb <phaze@duck.com>
2023-08-27 18:21:31 +0100

    fixed a few delete bugs

--- gui/src/photoUI/delete.ts
+++ gui/src/photoUI/delete.ts
@@ -126,7 +126,49 @@ let regButton = ( setTray: Function, trayOpen: () => string, currentPhoto: () =>
           }
         });
 
-        fetch('http://127.0.0.1:53413/api/v1/photos/'+currentPhoto().timestamp+'/delete', { headers: { key: localStorage.getItem('token')! } });
+        document.querySelector<HTMLElement>('.loading')!.style.display = 'block';
+        document.querySelector<HTMLElement>('.loading')!.innerHTML = 'Deleting Image...';
+
+        anime({
+          targets: '.loading',
+          opacity: 1,
+          easing: 'linear',
+          duration: 300
+        })
+
+        fetch('http://127.0.0.1:53413/api/v1/photos/'+currentPhoto().timestamp+'/delete', { headers: { key: localStorage.getItem('token')! } })
+          .then(data => data.json())
+          .then(data => {
+            if(!data.ok){
+              document.querySelector<HTMLElement>('.notification-error')!.innerHTML = data.message;
+
+              anime.set('.notification-error', { translateX: '-50%', translateY: '-50px' });
+              anime({
+                targets: '.notification-error',
+                opacity: 1,
+                translateY: 0,
+              })
+
+              setTimeout(() => {
+                anime({
+                  targets: '.notification-error',
+                  opacity: 0,
+                  translateY: '-50px',
+                })
+              }, 5000);
+            }
+
+            document.querySelector<HTMLElement>('.loading')!.innerHTML = 'Image Deleted.';
+            anime({
+              targets: '.loading',
+              opacity: 0,
+              duration: 300,
+              easing: 'linear',
+              complete: () => {
+                document.querySelector<HTMLElement>('.loading')!.style.display = 'none';
+              }
+            })
+          })
       }
 
       anime.set('.image-tray', { translateX: '-50%', translateY: '-20px' });
